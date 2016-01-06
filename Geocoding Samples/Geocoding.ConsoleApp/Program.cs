fdb5499658c540d09afd213687dc507d3c4bb257
J.T <gisfromscratch@live.de>
2016-01-06 20:31:39 +0100

    #1: Implemented the console app reading a reference dataset from CSV file

--- Geocoding Samples/Geocoding.ConsoleApp/Program.cs
+++ Geocoding Samples/Geocoding.ConsoleApp/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Geocoding.Core;
+using Geocoding.IO;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,17 +16,23 @@ namespace Geocoding.ConsoleApp
     {
         static void Main(string[] args)
         {
-            if (2 != args.Length)
+            if (4 != args.Length)
             {
-                Console.Error.WriteLine(@"Usage: -f <csv_file>");
+                Console.Error.WriteLine(@"Usage: -f <csv_file> -r <csv_reference_file>");
                 return;
             }
 
-            for (var index = 0; index < args.Length; index += 2)
+            var csvFileReader = new CsvFileReader();
+            ReferenceDataset referenceDataset = null;
+            for (var index = 0; index < args.Length - 1; index += 2)
             {
                 if (0 == string.Compare(args[index], @"-f"))
                 {
                     
+                }
+                else if (0 == string.Compare(args[index], @"-r"))
+                {
+                    referenceDataset = csvFileReader.ReadFile(new FileInfo(args[index + 1]), false, '\t');
                 }
             }
         }
