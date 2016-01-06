10108a9b3d1a96c6491e3827ae72c21e32d808e5
J.T <gisfromscratch@live.de>
2016-01-06 06:53:07 +0100

    #1: Added a CSV file reader with tests.

---
+++ Geocoding Samples/Geocoding.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geocoding.ConsoleApp
+{
+    /// <summary>
+    /// Represents a console applicaton used for geocoding files.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (2 != args.Length)
+            {
+                Console.Error.WriteLine(@"Usage: -f <csv_file>");
+                return;
+            }
+
+            for (var index = 0; index < args.Length; index += 2)
+            {
+                if (0 == string.Compare(args[index], @"-f"))
+                {
+                    
+                }
+            }
+        }
+    }
+}
---
+++ Geocoding Samples/Geocoding.IO.Testing/CsvFileReaderTestSuite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Geocoding.IO.Testing
+{
+    /// <summary>
+    /// Represents tests for reading CSV files.
+    /// </summary>
+    [TestClass]
+    public class CsvFileReaderTestSuite
+    {
+        [TestMethod]
+        public void TestCsvFileRead()
+        {
+            var reader = new CsvFileReader();
+            var dataset = reader.ReadFile(new FileInfo(@"data/ReferenceDataset1.csv"), true, ',');
+            Assert.AreEqual(1, dataset.Records.Count, @"Record count is wrong!");
+        }
+    }
+}
