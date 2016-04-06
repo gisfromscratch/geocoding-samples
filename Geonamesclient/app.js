0366b08f0dc9b02732f5978b376e0e81ff68c5b9
developer <gisfromscratch@live.de>
2016-04-07 00:39:25 +0200

    Geocodes a bunch of places and writes them into a feature json file

--- Geonamesclient/app.js
+++ Geonamesclient/app.js
@@ -1,3 +1,4 @@
+var countryCodes = require("i18n-iso-countries");
 var fileSystem = require("fs");
 var geode = require("geode");
 var readLine = require("readline");
@@ -5,18 +6,39 @@ var readLine = require("readline");
 console.log("Geosearch . . .");
 console.log(process.cwd());
 
-var fileInputPath = "Geonamesclient/placenames.txt";
+var fileInputPath = "placenames.txt";
 
-readLine.createInterface({
+var features = [];
+var fileOutputPath = "features.json";
+var writeStream = fileSystem.createWriteStream(fileOutputPath);
+var hasWrittenFeature = false;
+
+var readInterface = readLine.createInterface({
     input: fileSystem.createReadStream(fileInputPath),
     terminal: false
-}).on("line", function (placename) {
-    console.log(placename);
-    console.log();
+});
+readInterface.on("line", function (line) {
+    var splittedLine = line.split(",");
+    if (splittedLine.length < 8) {
+        return;
+    }
     
-    var geo = new geode("demo", {language: "en"});
+    var placename = splittedLine[1].trim(" ");
+    var countryName = splittedLine[2].trim(" ");
+    var language = "en";
+    var countryCode = countryCodes.getAlpha2Code(countryName, language);
+    
+    var geo = new geode("gisfromscratch", { 
+        language : language
+    });
+    
+    var searchOptions = {
+        name : placename,
+        country : countryCode,
+        maxRows : 1
+    };
 
-    geo.search({name : placename}, function(err, results) {
+    geo.search(searchOptions, function(err, results) {
         if (err) {
             console.error(err);
             return;
@@ -25,7 +47,7 @@ readLine.createInterface({
         var wgs84 = {
             wkid : 4326  
         };
-        var nextResult, jsonGeometry;
+        var nextResult, nextFeature, jsonGeometry, attributes;
         var geonames = results.geonames;
         for (var index in geonames) {
             nextResult = geonames[index];
@@ -34,7 +56,34 @@ readLine.createInterface({
                 y : nextResult.lat,
                 spatialReference : wgs84       
             };
-            console.log(JSON.stringify(jsonGeometry));
+            attributes = {
+                "date": splittedLine[0],
+                "city" : splittedLine[1],
+                "country" : splittedLine[2],
+                "perpetrator" : splittedLine[3],
+                "weapon" : splittedLine[4],
+                "injuries" : splittedLine[5],
+                "fatalities" : splittedLine[6],
+                "description" : splittedLine[7],
+                "geoname" : nextResult.name    
+            };
+            nextFeature = {
+                geometry : jsonGeometry,
+                attributes : attributes    
+            };
+            
+            var featureString = JSON.stringify(nextFeature);
+            if (hasWrittenFeature) {
+                featureString = "," + featureString;
+            }
+            var writeFailed = !writeStream.write(featureString);
+            if (writeFailed) {
+                writeStream.once("drain", function() {
+                    writeStream.write(featureString);
+                });
+            } else {
+                hasWrittenFeature = true;
+            }
         }
     });
 });
